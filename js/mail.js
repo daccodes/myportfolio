faa5111584249cf98d9c2e373579c91c00837984
dacmethod <80635684+dacmethod@users.noreply.github.com>
2021-06-27 11:20:55 +0200

    mod1

---
+++ js/mail.js
@@ -0,0 +1,26 @@
+(function() {
+    emailjs.init("user_XW2bGukwTaEOwzfUe5bf0");
+    })();
+
+    const btn = document.getElementById('button');
+
+    document.getElementById('contact-form')
+    .addEventListener('submit', function(event) {
+      event.preventDefault();
+
+      btn.value = 'Invio in corso...';
+
+      const serviceID = 'default_service';
+      const templateID = 'template_s2mqd7e';
+
+      emailjs.sendForm(serviceID, templateID, this)
+        .then(() => {
+          btn.value = 'Inviato!';
+          //alert('Inviato!');
+          location.reload();
+          
+        }, (err) => {
+          btn.value = 'Inviato!';
+          alert(JSON.stringify(err));
+        });
+    });
